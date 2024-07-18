dafed70ed5ad1abe8a36ec38f756d6eda4637406
ganes <ganes@LAPTOP-NDQD7FU3.mshome.net>
2024-07-18 09:57:01 +0530

    commit

---
+++ src/main/java/com/example/demo/RegisterloginFormApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterloginFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterloginFormApplication.class, args);
+	}
+
+}
